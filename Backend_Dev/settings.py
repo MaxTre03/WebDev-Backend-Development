e9cea457cba1264fd41157d117a7257ae3e31fb2
slg4890 <ryan.birge01@utrgv.edu>
2026-04-25 12:09:05 -0500

    initial setup, created database, linked it, and setup .env file

--- Backend_Dev/settings.py
+++ Backend_Dev/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Backend_Dev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Kita3080$',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
